52832feafae261d0c18956bb2a88511536fc1bff
nathan findley <nathan@zenlok.com>
2016-03-08 10:47:13 +0900

    Updated testing.

--- main_test.go
+++ main_test.go
@@ -6,11 +6,17 @@ import (
 
 func TestTrimArguments(t *testing.T) {
 	args := []string{"program", "b", "c", "d", "e"}
-	trimmed := trimArguments(args)
+	trimmed, config := trimArguments(args)
 	if len(trimmed) != 2 {
 		t.Errorf("Bad arguments returned %+v", trimmed)
 	}
 	if trimmed[0] != "b" || trimmed[1] != "e" {
+		t.Errorf("Bad arguments returned %+v", trimmed)
+	}
+	if len(config) != 3 {
+		t.Errorf("Bad config arguments returned %+v", config)
+	}
+	if config[0] != "program" || config[1] != "c" || config[2] != "d" {
 		t.Errorf("Bad arguments returned %+v", trimmed)
 	}
 }
