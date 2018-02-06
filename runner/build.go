7ee465e96031ebf5259635c61a5a017544b38ade
Nate <nathan@4ydx.com>
2018-02-06 22:21:25 +0900

    Js build.

--- runner/build.go
+++ runner/build.go
@@ -7,26 +7,19 @@ import (
 	"os/exec"
 )
 
-func build() (string, bool) {
-	buildLog("Building...")
-
-	cmd := exec.Command("go", "build", "-o", buildPath(), root())
-
+func runBuild(cmd *exec.Cmd) (string, bool) {
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		fatal(err)
 	}
-
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		fatal(err)
 	}
-
 	err = cmd.Start()
 	if err != nil {
 		fatal(err)
 	}
-
 	io.Copy(os.Stdout, stdout)
 	errBuf, _ := ioutil.ReadAll(stderr)
 
@@ -34,6 +27,57 @@ func build() (string, bool) {
 	if err != nil {
 		return string(errBuf), false
 	}
+	return "", true
+}
 
+func build() (string, bool) {
+	buildLog("Building...")
+
+	cmd := exec.Command("go", "build", "-o", buildPath(), root())
+	msg, ok := runBuild(cmd)
+	if !ok {
+		return msg, ok
+	}
+	cmd = exec.Command("make")
+	msg, ok = runBuild(cmd)
+	if !ok {
+		return msg, ok
+	}
+	_, err := os.Stat("static/js/build.sh")
+	if err == nil {
+		buildLog("Building js chdir...")
+
+		err = os.Chdir("static/js/")
+		if err != nil {
+			fatal(err)
+		}
+
+		buildLog("Building js...")
+		cmd := exec.Command("./build.sh")
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			fatal(err)
+		}
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			fatal(err)
+		}
+		err = cmd.Start()
+		if err != nil {
+			fatal(err)
+		}
+		io.Copy(os.Stdout, stdout)
+		errBuf, _ := ioutil.ReadAll(stderr)
+
+		buildLog("Building js retreat...")
+		err = os.Chdir("../../")
+		if err != nil {
+			fatal(err)
+		}
+		err = cmd.Wait()
+		if err != nil {
+			return string(errBuf), false
+		}
+	}
 	return "", true
 }
